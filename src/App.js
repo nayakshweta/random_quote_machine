56f190b805b3e08974eaf6bcef94c13925200320
Shweta <nayakshweta07@gmail.com>
2020-03-11 03:48:16 +0530

    Implements random quote generator using React.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class QuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteObj: {}
+    }
+    
+    this.getQuote = this.getQuote.bind(this);
+  }
+  componentDidMount() {
+    this.getQuote();
+  }
+  
+  getQuote() {
+    fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ quoteObj: data})
+    })
+  }
+  
+  render() {
+    return (
+      <h2 id="quote-box">
+        <p id="text">{this.state.quoteObj.content}</p>
+        <p id="author">{this.state.quoteObj.author}</p>
+        <br/>
+        <a id="new-quote" onClick={this.getQuote}>Get New Quote</a>&nbsp;
+        <a href="https://twitter.com/intent/tweet" id="tweet-quote">Link to tweet</a>
+      </h2>
+    );
+  }
 }
 
-export default App;
+
+export default QuoteMachine;
